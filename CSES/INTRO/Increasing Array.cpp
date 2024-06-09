d78fe761455c1a09b137b9edf30d79222dbd7c08
SatvikGautam1185 <153541621+SatvikGautam1385@users.noreply.github.com>
2024-06-09 21:23:03 +0530

    Update Increasing Array.cpp

--- CSES/INTRO/Increasing Array.cpp
+++ CSES/INTRO/Increasing Array.cpp
@@ -1,30 +1,34 @@
+//focus 
+//debugging
+//dry run
+
 #include<iostream>
 using namespace std;
-
+ 
 int main()
 {
-        long long int p;
-        cin >> p;
-
-        const long long int n = p;
-
+	long long int p;
+	cin >> p;
+ 
+	const long long int n = p;
+ 
         int arr[n];
-
-        for(long long int i = 0; i<n; i++)
-        {
-                cin >> arr[i];
-        }
-
-        long long int x = 0;
-
-        for(long long int i = 1; i<=n; i++)
-        {
-                if(arr[i] < arr[i-1])
-                {
-                        arr[i] = arr[i-1];
-                        x = x + ( arr[i-1] - arr[i] );
-                }
-        }
-
-        cout << x;
+ 
+	for(long long int i = 0; i<n; i++)
+	{
+		cin >> arr[i];
+	}
+ 
+	long long int x = 0;
+ 
+	for(long long int i = 1; i<n; i++)
+	{
+		if(arr[i] < arr[i-1])
+		{
+			x = x + ( arr[i-1] - arr[i] );
+			arr[i] = arr[i-1];
+		}
+	}
+ 
+	cout << x;
 }
