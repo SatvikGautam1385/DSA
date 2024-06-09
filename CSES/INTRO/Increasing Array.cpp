947702a5d4896e0271e6c3238bc80208646ea9d7
SatvikGautam1385 <153541621+SatvikGautam1385@users.noreply.github.com>
2024-06-09 21:15:05 +0530

    Create Increasing Array.cpp

---
+++ CSES/INTRO/Increasing Array.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+        long long int p;
+        cin >> p;
+
+        const long long int n = p;
+
+        int arr[n];
+
+        for(long long int i = 0; i<n; i++)
+        {
+                cin >> arr[i];
+        }
+
+        long long int x = 0;
+
+        for(long long int i = 1; i<=n; i++)
+        {
+                if(arr[i] < arr[i-1])
+                {
+                        arr[i] = arr[i-1];
+                        x = x + ( arr[i-1] - arr[i] );
+                }
+        }
+
+        cout << x;
+}
